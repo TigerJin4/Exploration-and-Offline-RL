e65ce0ebe77deaef1449e9890ff03f0767335e94
Wenhan Jin <tigerjin4@berkeley.edu>
2020-11-28 16:54:24 -0800

    first commit

---
+++ policies/argmax_policy.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pdb
+
+
+class ArgMaxPolicy(object):
+
+    def __init__(self, critic):
+        self.critic = critic
+
+    def set_critic(self, critic):
+        self.critic = critic
+
+    def get_action(self, obs):
+        # MJ: changed the dimension check to a 3
+        if len(obs.shape) > 3:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        ## TODO return the action that maxinmizes the Q-value 
+        # at the current observation as the output
+        q_values = self.critic.qa_values(observation)
+
+        action = q_values.argmax(-1)
+        return action[0]
+      
+    def get_q_values(self, obs):
+        # MJ: changed the dimension check to a 3
+        if len(obs.shape) > 3:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        ## TODO return the action that maxinmizes the Q-value 
+        # at the current observation as the output
+        q_values = self.critic.qa_values(observation)
+        return q_values
+
+    ####################################
+    ####################################
